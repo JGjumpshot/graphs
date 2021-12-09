b950396db284c4de9157d871ea47888f0d99f0a3
Jackson Gunther <jacksongunther@filevine.com>
2021-12-08 21:36:20 -0700

    updates

--- graph.py
+++ graph.py
@@ -86,7 +86,9 @@ class Graph:
         return iter(visited)
     def __str__(self):
         for node in self.graph_dict:
-            print(self.graph_dict[node])
+            print(self.graph_dict[node].id, end=" ")
+            for neighbor in self.graph_dict[node].connected_to:
+                print(neighbor)
         formatted_str = f"digraph G {{\n\n}}"
         return formatted_str
     def __iter__(self):
